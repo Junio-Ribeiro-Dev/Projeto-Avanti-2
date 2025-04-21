861bd2b7a92d0f945e526bafb04fad6c18a63cd1
Junio-Ribeiro-Dev <junioibgs@gmail.com>
2025-04-21 12:50:00 -0300

    att do projeto 2

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,18 @@ import { Outlet } from "react-router-dom"
 
 import classes from './App.module.css'
 
+import ellipse1 from './img/Ellipse 1.svg'
+import ellipse2 from './img/Ellipse 2.svg'
+
+import camada from './img/Camada_1.svg'
 function App() {
 
 
   return (
     <div className={classes.app}>
-      
+      <img className={classes.e1}  src={ellipse1} alt="elipse" />
+      <img className={classes.e2}  src={ellipse2} alt="elipse2" />
+      <img className={classes.camada}  src={camada} alt="camada" />
     <Outlet />
     </div>
   )
